2f9b5ca57b8df592f2a1dfe70bc75380f6e092fe
Santiago <santiagofgaray@gmail.com>
2022-01-28 07:06:38 +0100

    first commit

---
+++ scripts/components.js
@@ -0,0 +1,45 @@
+class Component {
+    constructor(width, height,color, x, y){
+    this.width = width;
+    this.height = height;
+    this.img = color;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+    }
+
+    update() {
+        const ctx = myGameArea.context = myGameArea.context;
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+    newPos() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+      }
+
+      left() {
+        return this.x;
+      }
+      right() {
+        return this.x + this.width;
+      }
+      top() {
+        return this.y;
+      }
+      bottom() {
+        return this.y + this.height;
+      }
+
+      crashWith(obstacle) {
+        return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
+      }
+
+
+}
+
+const player = new Component(30, 30, 'red', 0 , 110)
+
+const myObstacles = [];
